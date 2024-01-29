163634fbca324bc574460c78bca6d0f1b65afc94
KrutiMistry094 <krutirajnikant.mistry@edu.sait.ca>
2024-01-29 15:58:38 -0700

    shopping list

---
+++ app/week-2/student-info.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+  return (
+    <main>
+      <p>Kruti Mistry</p>
+      <Link href="https://github.com/KrutiMistry094?tab=repositories">
+        Github repo
+      </Link>
+    </main>
+  );
+}
